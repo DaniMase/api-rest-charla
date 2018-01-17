16b105a3062a5b52b6d320b37ed6ea0624f78d56
DaniMase <danielmasegosa@hotmail.com>
2018-01-18 00:05:13 +0100

    Conflicto

--- db/warehouse_db_connect.js
+++ db/warehouse_db_connect.js
@@ -12,4 +12,6 @@ const warehouse_conn = mongoose.createConnection(process.env.MONGODB_WAREHOUSE_U
 
 const Warehouse = warehouse_conn.model('Warehouse', WarehouseSchema);
 
+//Quiero generar un conflicto
+
 module.exports = {Warehouse};
