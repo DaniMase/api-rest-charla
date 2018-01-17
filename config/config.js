dc544f4e732f50c232022d229ea6eb62e2d39cf0
Daniel Masegosa <daniel.masegosa.contractor@bbva.com>
2018-01-17 15:44:50 +0100

    Catch not found config

--- config/config.js
+++ config/config.js
@@ -1,11 +1,15 @@
 let env = process.env.NODE_ENV || 'development';
 
 if (env === 'development' || env === 'test') {
-    let config = require('./config.json');
+    try{
+        let config = require('./config.json');
 
-    let envConfig = config[env];
+        let envConfig = config[env];
 
-    Object.keys(envConfig).forEach((key) => {
-        process.env[key] = envConfig[key];
-    })
+        Object.keys(envConfig).forEach((key) => {
+            process.env[key] = envConfig[key];
+        });
+    }catch(e){
+        console.log('Not configuration file find, using env variables');
+    }
 }
